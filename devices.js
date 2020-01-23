f23db0a6ddba0fc9d677496ce6002ae0e05a45d6
Peter deHaan <peter@deseloper.com>
2020-01-23 12:04:47 -0800

    Add devices script

---
+++ devices.js
@@ -0,0 +1,7 @@
+const pw = require("playwright");
+const {name, version} = require("playwright/package.json");
+
+console.log(`${name}@${version}`);
+for (const device of pw.devices) {
+  console.log(`${device.name.padEnd(30, ".")} ${JSON.stringify(device.viewport)}`);
+}
